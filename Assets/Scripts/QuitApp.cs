f09936c88b8e19e9dc1a9d0aacd63d865a028ec0
Aserback <33083466+Aserback@users.noreply.github.com>
2017-10-27 22:12:53 +0200

    Initialer Commit

---
+++ Assets/Scripts/QuitApp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApp : MonoBehaviour
+{
+
+  public void Quit() {
+    UnityEditor.EditorApplication.isPlaying = false;
+
+    Application.Quit();
+  }
+
+}
---
+++ Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour
+{
+  public EventSystem eventSystem;
+  public GameObject selectedObject;
+
+  private bool isSelected;
+
+  void Update() {
+    if (Input.GetAxisRaw("Vertical") != 0 && !isSelected) {
+      eventSystem.SetSelectedGameObject(selectedObject);
+      isSelected = true;
+    }
+  }
+
+  private void OnDisable() {
+    isSelected = false;
+  }
+}
